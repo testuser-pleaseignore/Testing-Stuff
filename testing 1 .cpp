8e19132b096eb98f533c9c36fe570762459af690
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-01-14 11:45:12 +0700

    long long

---
+++ testing 1 .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int n;
+cout << "enter n: ";
+cin >> n;
+for(int i =1;i<=12;i++){
+    cout << n << " x " << i << " = " << n*i << endl;
+}
+    return 0;
+}
